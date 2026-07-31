29cee71ef4b9cd5d24c2b1a770845415b4220552
How346 <How@pindush.click>
2026-07-31 22:07:53 +0530

    Update game_config.dart

--- lib/config/game_config.dart
+++ lib/config/game_config.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class GameConfig {
-  static const String appName = 'GridSnap';
-  static const int maxHearts = 3;
-  static const int maxLevels = 100;
-
-  // Colors & Aesthetic
-  static const Color primaryColor = Color(0xFF6C5CE7);
-  static const Color secondaryColor = Color(0xFFA29BFE);
-  static const Color bgGradientStart = Color(0xFF0F0C29);
-  static const Color bgGradientEnd = Color(0xFF24243E);
-  static const Color glassBorderColor = Colors.white24;
-  static const Color accentColor = Color(0xFF00CEC9);
-  static const Color blockColor = Color(0xFF2A2750);
-  static const Color highlightColor = Color(0xFFFFEAA7);
-}
