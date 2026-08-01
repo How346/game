633b0a44c191fbfe51aa126471440eb473c56ef2
How346 <How@pindush.click>
2026-08-01 18:40:34 +0530

    Update storage_service.dart

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,32 +1,34 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageService {
-  static const String _keyUnlockedLevel = 'unlocked_level';
-  static const String _keySound = 'sound_enabled';
-
   static Future<int> getUnlockedLevel() async {
     final prefs = await SharedPreferences.getInstance();
-    // Defaults to level 1 if no data is saved yet
-    return prefs.getInt(_keyUnlockedLevel) ?? 1;
+    return prefs.getInt('unlocked_level') ?? 1;
   }
 
   static Future<void> saveUnlockedLevel(int level) async {
     final prefs = await SharedPreferences.getInstance();
     int currentUnlocked = await getUnlockedLevel();
-    
-    // Only save if the new level is higher than the currently unlocked level
-    if (level > currentUnlocked) {
-      await prefs.setInt(_keyUnlockedLevel, level);
+    if (level > currentUnlocked) await prefs.setInt('unlocked_level', level);
+  }
+
+  static Future<bool> isSoundEnabled() async => (await SharedPreferences.getInstance()).getBool('sound') ?? true;
+  static Future<void> setSoundEnabled(bool v) async => (await SharedPreferences.getInstance()).setBool('sound', v);
+
+  static Future<bool> isHapticEnabled() async => (await SharedPreferences.getInstance()).getBool('haptic') ?? true;
+  static Future<void> setHapticEnabled(bool v) async => (await SharedPreferences.getInstance()).setBool('haptic', v);
+
+  static Future<bool> isDarkMode() async => (await SharedPreferences.getInstance()).getBool('dark_mode') ?? false;
+  static Future<void> setDarkMode(bool v) async => (await SharedPreferences.getInstance()).setBool('dark_mode', v);
+
+  static Future<int> getBestSteps(int level) async => (await SharedPreferences.getInstance()).getInt('best_steps_$level') ?? 999;
+  static Future<bool> saveBestSteps(int level, int steps) async {
+    final prefs = await SharedPreferences.getInstance();
+    int currentBest = await getBestSteps(level);
+    if (steps < currentBest) {
+      await prefs.setInt('best_steps_$level', steps);
+      return true; // New best!
     }
-  }
-
-  static Future<bool> isSoundEnabled() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_keySound) ?? true;
-  }
-
-  static Future<void> setSoundEnabled(bool enabled) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_keySound, enabled);
+    return false;
   }
 }
